47b19f4eb2efde0ab0f37ebe8d457991f4d82856
Alexandr Eremeev <eremeev.a.e@mail.ru>
2024-04-14 22:43:39 +0700

    refactor: main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,9 @@
-import psycopg2
 import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from schema import schema
+
 from dotenv import load_dotenv
 
 load_dotenv()
-
-
-print(os.environ.get('DB_HOST'))
-
-def connect_to_db():
-    connection = psycopg2.connect(
-        host=os.environ.get('DB_HOST'),
-        database=os.environ.get('DB_NAME'),
-        user=os.environ.get('DB_LOGIN'),
-        password=os.environ.get('DB_PASSWORD'),
-    )
-    print("connect to the database")
-    connection.close()
-
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -27,11 +12,7 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         if self.path == '/graphql':
-            query = """{
-                hello
-            }"""
-
-            print(schema.execute(query).data)
+            print('graphql')
 
 def run(server_class=HTTPServer, handler_class=HTTPRequestHandler):
     server_address = (os.environ.get('APP_HOST'), 80)
--- app/schema.py
+++
@@ -1,9 +0,0 @@
-import graphene
- 
-class Query(graphene.ObjectType):
-    hello = graphene.String()
- 
-    def resolve_hello(self, info):
-        return "Hello, world!"
- 
-schema = graphene.Schema(query=Query)
